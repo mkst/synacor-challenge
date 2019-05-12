74700d40ed809ed44c77d0ac0744d97efa25e3db
Mark Street <streetster@gmail.com>
2019-05-12 22:44:47 +0100

    Adding 6th code

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # synacor-challenge
+
 An incomplete solution to the [2012 Synacor Challenge](http://challenge.synacor.com) written in Python.
 
 ## Codes
+
 Codes discovered so far:
 
  - `fbwwsldZmWKv` (arch-spec)
  - `wLCVsTGpgzqR` (pre-selftest)
  - `KCojPVcoOdVP` (post-selftest)
  - `AqxdDiCYrzVE` ("use tablet")
- - `EjNzmoeZrZQC` ("use teleporter")
+ - `EjNzmoeZrZQC` ("use teleporter" when broken)
+ - `????????????` ("use teleporter" once fixed)
+ - `dxVYIIAbxMl8` => `bxVYIIAdxMl8` ("use mirror")
 
-## Coins
+## Walkthrough
+
+Commands can be found in [walkthrough.txt](/artefacts/walkthru.txt)
+
+### Coins
 
 ```
 _ + _ * _^2 + _^3 - _ = 399
@@ -27,3 +35,37 @@ _ + _ * _^2 + _^3 - _ = 399
 ```
 9 + 2 * 5^2 + 7^3 - 3 = 399
 ```
+
+### Vault
+
+```
+-------------------------------------
+|   *    |   8    |   -    |  1 (V) |
+-------------------------------------
+|   4    |   *    |   11   |   *    |
+-------------------------------------
+|   +    |   4    |   -    |   18   |
+------------------------------------
+| 22 (O) |   -    |   9    |   *    |
+-------------------------------------
+```
+
+Orb is `22`
+
+Vault requires `30`
+
+**Steps:**
+ - north, east, 22 + 4 => 26
+ - east, north, 26 - 11 => 15
+ - west, south, 15 * 4 => 60
+ - east, east, 60 - 18 => 42
+ - west, north, 42 - 11 => 31
+ - north, east, 31 - 1 => 30
+
+**Extra:**
+- `memory[3953]` keeps track of number of moves
+- `memory[3952]` keeps track of orb value
+
+## Notes
+
+ - Journal entry days are the start of the Fibonacci sequence
--- synacor.py
+++ synacor.py
@@ -1,208 +1,261 @@
+"""This module is an incomplete implementation of the VM required to solve the Synacor challenge"""
+
 import struct # for pack, unpack
 import sys    # for exit
 
-class vm:
-    # define memory
-    memory = None
-    # initialise registers to zero
-    register = 8*[0]
-    # initialise empty stack
-    stack = []
-    # initialise instruction pointer
-    ptr = 0
-    # buffers for std in and out
-    stdout = []
-    stdin = []
-    #
-    debuglog = None
-    debug = False
-    # 15-bit addressable space
-    MAXINT = pow(2, 15)
+# 15-bit addressable space
+MAXINT = pow(2, 15)
 
-    # returns value in register or literal
-    def _tryregister(self, x):
-        return x if x < self.MAXINT else self.register[x - self.MAXINT]
+# pylint: disable=too-many-instance-attributes,too-few-public-methods
+class Synacor(object):
+    """The Synacore Virtual Machine"""
 
-    # writes value y to register x
-    def _register(self, x,y):
-        self.register[x - self.MAXINT] = int(y)
+    def _try_register(self, x):
+        """returns value in register or literal"""
+        return x if x < MAXINT else self.register[x - MAXINT]
 
-    #print to screen if _debug is enabled
-    def _dbg(self, x):
-        if self.debuglog is None:
-            self.debuglog = open("debug.log","a")
-        self.debuglog.write(str(self.ptr) + "\t"  + " ".join(x) + "\n")
+    def _register(self, x, y):
+        """writes value y to register x"""
+        self.register[x - MAXINT] = int(y)
 
     def _stdout(self, x):
+        """append to stdout buffer, print if newline"""
         if x == ord("\n"):
             print "".join(self.stdout)
-            self.stdout = []
+            self.stdout = list()
         else:
             self.stdout.append(chr(x))
 
     def _stdin(self):
+        """read into stdin buffer if empty, else pop stdin buffer"""
+        def user_input():
+            """read user input"""
+            raw = raw_input()
+            ui = raw.split(" ")
+            return [raw, ui[0], ui[1:]]
         if len(self.stdin) == 0:
-            ui = raw_input()
-            # custom debug commands
-            while ui.split(" ")[0] in ["dump", "load", "debug", "registers", "exit", "fix", "peek", "poke"]:
-                args = ui.split(" ")
-                cmd = args[0]
-                if cmd == "dump":
-                    with open ("memory.dmp","wb") as s:
-                        s.write(struct.pack("<" + "H" * len(self.memory), *self.memory))
-                        print "Memory dumped."
-                    with open ("registers.dmp","wb") as s:
-                        s.write(struct.pack("<" + "H" * 8, *self.register))
-                        print "Registers dumped."
-                    with open ("stack.dmp","wb") as s:
-                        s.write(struct.pack("<" + "H" * len(self.stack), *self.stack))
-                        print "Stack dumped."
-                    with open ("ptr.dmp","wb") as s:
-                        s.write(struct.pack("<" + "H", self.ptr))
-                        print "Ptr dumped."
-                elif cmd == "load":
-                    with open ("memory.dmp","rb") as s:
-                        d = s.read()
-                        self.memory = [x for x in struct.unpack("<" + "H" * int(0.5*len(d)), d)]
-                    with open ("registers.dmp","rb") as s:
-                        d = s.read()
-                        self.register = [x for x in struct.unpack("<" + "H" * 8, d)]
-                    with open ("stack.dmp","rb") as s:
-                        d = s.read()
-                        self.stack = [x for x in struct.unpack("<" + "H" * int(0.5*len(d)),d)]
-                    with open ("ptr.dmp","rb") as s:
-                        self.ptr = struct.unpack("<H", s.read())[0]
-                elif cmd == "fix":
-                    print "'Fixing' teleporter"
-                    self._fixteleporter()
-                elif cmd == "debug":
-                    print "Debug: " + ("off" if self.debug else "on")
-                    self.debug = ~self.debug
-                elif cmd == "registers":
-                    print "Registers",[x for x in self.register]
-                elif cmd == "peek":
-                    if len(args) == 3:
-                        self._peek(*[int(x) for x in args[1:]])
-                    else:
-                        print "peek requires address and length"
-                elif cmd == "poke":
-                    if len(args) == 3:
-                        self._poke(*[int(x) for x in args[1:]])
-                    else:
-                        print "poke requires address and value"
-                elif cmd == "exit":
-                    sys.exit()
+            raw, cmd, args = user_input()
+            while cmd in self.own_instructions:
+                try:
+                    self.own_instructions[cmd](self, *args)
+                except TypeError as e:
+                    print e
                 print "What do you do?"
-                ui = raw_input()
-            self.stdin = [ord(x) for x in ui + "\n"]
+                raw, cmd, args = user_input()
+            self.stdin = [ord(x) for x in raw + "\n"]
         return self.stdin.pop(0) # pop front of queue
 
-    # stop execution and terminate the program
     def _halt(self):
-      self.running = False
-    # set register <a> to the value of <b>
+        """stop execution and terminate the program"""
+        self.running = False
     def _set(self, a, b):
-      self._register(a,self._tryregister(b))
-    # push <a> onto the stack
+        """set register <a> to the value of <b>"""
+        self._register(a, self._try_register(b))
     def _push(self, a):
-      self.stack.append(self._tryregister(a))
-    # remove the top element from the stack and write it into <a>; empty stack = error
+        """push <a> onto the stack"""
+        self.stack.append(self._try_register(a))
     def _pop(self, a):
-      self._register(a,self.stack.pop())
-    # set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise
+        """remove the top element from the stack and write it into <a>"""
+        self._register(a, self.stack.pop())
     def _eq(self, a, b, c):
-      self._register(a, self._tryregister(b) == self._tryregister(c))
-    # set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
+        """set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise"""
+        self._register(a, self._try_register(b) == self._try_register(c))
     def _gt(self, a, b, c):
-      self._register(a, self._tryregister(b) > self._tryregister(c))
-    # jump to <a>
+        """set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise"""
+        self._register(a, self._try_register(b) > self._try_register(c))
     def _jmp(self, a):
-      return self._tryregister(a)
-    # jump-true, if <a> is nonzero, jump to <b>
+        """jump to <a>"""
+        return self._try_register(a)
     def _jt(self, a, b):
-      return self._tryregister(b) if self._tryregister(a) != 0 else None
-    # jump-false, if <a> is zero, jump to <b>
+        """jump-true, if <a> is nonzero, jump to <b>"""
+        return self._try_register(b) if self._try_register(a) != 0 else None
     def _jf(self, a, b):
-      return self._tryregister(b) if self._tryregister(a) == 0 else None
-    # assign into <a> the sum of <b> and <c> (modulo 32768)
+        """jump-false, if <a> is zero, jump to <b>"""
+        return self._try_register(b) if self._try_register(a) == 0 else None
     def _add(self, a, b, c):
-      self._register(a, (self._tryregister(b) + self._tryregister(c)) % self.MAXINT)
-    # store into <a> the product of <b> and <c> (modulo 32768)
+        """assign into <a> the sum of <b> and <c> (modulo 32768)"""
+        self._register(a, (self._try_register(b) + self._try_register(c)) % MAXINT)
     def _mult(self, a, b, c):
-      self._register(a, (self._tryregister(b) * self._tryregister(c)) % self.MAXINT)
-    # store into <a> the remainder of <b> divided by <c>
+        """store into <a> the product of <b> and <c> (modulo 32768)"""
+        self._register(a, (self._try_register(b) * self._try_register(c)) % MAXINT)
     def _mod(self, a, b, c):
-      self._register(a,self._tryregister(b) % self._tryregister(c))
-    # stores into <a> the bitwise and of <b> and <c>
+        """store into <a> the remainder of <b> divided by <c>"""
+        self._register(a, self._try_register(b) % self._try_register(c))
     def _and(self, a, b, c):
-      self._register(a,self._tryregister(b) & self._tryregister(c))
-    # stores into <a> the bitwise or of <b> and <c>
+        """store into <a> the bitwise and of <b> and <c>"""
+        self._register(a, self._try_register(b) & self._try_register(c))
     def _or(self, a, b, c):
-      self._register(a,self._tryregister(b) | self._tryregister(c))
-    # stores 15-bit bitwise inverse of <b> in <a>
+        """store into <a> the bitwise or of <b> and <c>"""
+        self._register(a, self._try_register(b) | self._try_register(c))
     def _not(self, a, b):
-      self._register(a,0x7fff & ~self._tryregister(b))
-    # read memory at address <b> and write it to <a>
+        """store 15-bit bitwise inverse of <b> in <a>"""
+        self._register(a, 0x7fff & ~self._try_register(b))
     def _rmem(self, a, b):
-      self._register(a,self.memory[self._tryregister(b)])
-    # write the value from <b> into memory at address <a>
+        """read memory at address <b> and write it to <a>"""
+        self._register(a, self.memory[self._try_register(b)])
     def _wmem(self, a, b):
-      self.memory[self._tryregister(a)] = self._tryregister(b)
-    # write the address of the next instruction to the stack and jump to <a>
+        """write the value from <b> into memory at address <a>"""
+        #if a == 3952:
+        #    print "orb value ",self._try_register(b)
+        #elif a == 3953:
+        #    print "steps taken",self._try_register(b)
+        self.memory[self._try_register(a)] = self._try_register(b)
     def _call(self, a):
-      self.stack.append(self.ptr + 1)
-      return self._tryregister(a)
-    # remove the top element from the stack and jump to it; empty stack = halt
+        """write the address of the next instruction to the stack and jump to <a>"""
+        self.stack.append(self.ptr + 2) # call takes 1 argument
+        return self._try_register(a)
     def _ret(self):
-      return self.stack.pop()
-    # write the character represented by ascii code <a> to the terminal
+        """remove the top element from the stack and jump to it"""
+        return self.stack.pop()
     def _out(self, a):
-      self._stdout(self._tryregister(a))
-    # read a character from the terminal and write its ascii code to <a>
+        """push character represented by ascii code <a> to stdout buffer"""
+        self._stdout(self._try_register(a))
     def _in(self, a):
-      self._register(a,self._stdin())
-    # no operation
-    def _noop(self):
-      return
+        """pop character from stdin buffer and write its ascii code to <a>"""
+        self._register(a, self._stdin())
 
-    def _fixteleporter(self):
-      self.register[7] = 1 # correct value TBD
-      #self.memory[6027] = 8 # change jt 32768 6035 => jf 32768 6035 (skip teleporter sanity)
+    # custom functions
+    def _dump(self):
+        """dump out memory, registers, stack and instruction pointer to disk"""
+        def dump(filename, data):
+            """inner dump function"""
+            with open(filename, "wb") as save:
+                save.write(data)
+        dump("memory.dmp",
+             struct.pack("<" + "H" * len(self.memory), *self.memory))
+        dump("register.dmp",
+             struct.pack("<" + "H" * len(self.register), *self.register))
+        dump("stack.dmp",
+             struct.pack("<" + "H" * len(self.stack), *self.stack))
+        dump("ptr.dmp",
+             struct.pack("<" + "H", self.ptr))
+
+    def _load(self):
+        """load in memory, registers, stack and instruction pointer from disk"""
+        def load(filename, unpack):
+            """inner load function"""
+            with open(filename, "rb") as save:
+                dump = save.read()
+                return list(struct.unpack(unpack, dump))
+        self.memory = load("memory.dmp",
+                           "<" + "H" * len(self.memory))
+        self.register = load("register.dmp",
+                             "<" + "H" * len(self.register))
+        self.stack = load("stack.dmp",
+                          "<" + "H" * len(self.stack))
+        self.ptr = load("ptr.dmp",
+                        "<" + "H")[0]
+    def _fix_teleporter(self):
+        print "'Fixing' teleporter..."
+        self.register[7] = 1 # correct value TBD, wrong value => incorrect code
+        self._poke(6027, 8) # change jt R0 6035 => jf R0 6035 (skip teleporter sanity)
+        self._poke(5495, 7) # change jf R1 5579 to jt R1 5579 (skip valid value check)
+    def _toggle_debug(self):
+        print "Debug " + ("off" if self.debug else "on")
+        self.debug = ~self.debug
+    def _print_registers(self):
+        print "Registers", self.ptr, [x for x in self.register]
     def _peek(self, address, length):
-      print " ".join([str(x) for x in self.memory[address:address+length]])
+        print " ".join(str(x) for x in self.memory[int(address):int(address) + int(length)])
     def _poke(self, address, value):
-      print "Poke: " + str(address) + " = " + str(value)
-      self.memory[address] = value
+        print "Poke: " + str(address) + " = " + str(value)
+        self._wmem(int(address), int(value))
+    def _dump_known_instructions(self):
+        with open("instructions.txt", "w") as dmp:
+            for _, v in sorted(self.known_instructions.iteritems()):
+                dmp.write(v + "\n")
 
-    instructionset = [_halt, _set, _push, _pop, _eq, _gt, _jmp, _jt, _jf, _add, _mult, _mod, _and, _or, _not, _rmem, _wmem, _call, _ret, _out, _in, _noop]
-    arguments =      [0,     2,    1,     1,    3,   3,   1,    2,   2,   3,    3,     3,    3,    3,   2,    2,     2,     1,     0,    1,    1,   0]
-    names =          ["halt","set","push","pop","eq","gt","jmp","jt","jf","add","mult","mod","and","or","not","rmem","wmem","call","ret","out","in","noop"]
+    own_instructions = dict([("dump", _dump),
+                             ("load", _load),
+                             ("fix", _fix_teleporter),
+                             ("debug", _toggle_debug),
+                             ("registers", _print_registers),
+                             ("peek", _peek),
+                             ("poke", _poke),
+                             ("dki", _dump_known_instructions),
+                             ("exit", sys.exit)])
 
-    # public methods
-    def loadmemory(self,memory):
+    instructions = [[_halt, 0, "halt"], # 0
+                    [_set, 2, "set"],   # 1
+                    [_push, 1, "push"], # 2
+                    [_pop, 1, "pop"],   # 3
+                    [_eq, 3, "eq"],     # 4
+                    [_gt, 3, "gt"],     # 5
+                    [_jmp, 1, "jmp"],   # 6
+                    [_jt, 2, "jt"],     # 7
+                    [_jf, 2, "jf"],     # 8
+                    [_add, 3, "add"],   # 9
+                    [_mult, 3, "mult"], # 10
+                    [_mod, 3, "mod"],   # 11
+                    [_and, 3, "and"],   # 12
+                    [_or, 3, "or"],     # 13
+                    [_not, 2, "not"],   # 14
+                    [_rmem, 2, "rmem"], # 15
+                    [_wmem, 2, "wmem"], # 16
+                    [_call, 1, "call"], # 17
+                    [_ret, 0, "ret"],   # 18
+                    [_out, 1, "out"],   # 19
+                    [_in, 1, "in"],     # 20
+                    [lambda x: None, 0, "noop"]
+                   ]
+
+    def _create_debug_string(self, instruction_name, args):
+        debug_string = ""
+        debug_string += str(self.ptr).ljust(6)
+        debug_string += instruction_name.ljust(5)
+        regs = [x if x < MAXINT else "R" + str(x-MAXINT) for x in args]
+        debug_string += " ".join(str(x).ljust(6) for x in regs).ljust(20)
+        debug_string += "| "
+        debug_string += " ".join(str(x).ljust(6) for x in self.register)
+        return debug_string
+
+    def _dbg(self, instruction_name, args):
+        """write debug info to debug log"""
+        if not self.debug_file:
+            self.debug_file = open("debug.log", "a")
+        self.debug_file.write(self._create_debug_string(instruction_name, args) + "\n")
+
+    def __init__(self, memory, debug=False):
+        self.running = False
+        # initialise memory
         self.memory = memory
+        # initialise registers to zero
+        self.register = 8*[0]
+        # initialise empty stack
+        self.stack = list()
+        # initialise instruction pointer
+        self.ptr = 0
+        # buffers for std in and out
+        self.stdout = list()
+        self.stdin = list() # no need for collections.deque
+        # debug settings
+        self.debug_file = None
+        self.debug = debug
+        self.known_instructions = dict()
 
     def run(self):
+        """Start up the Virtual Machine"""
         self.running = True
-        while (self.running):
-            i = self.memory[self.ptr]
-            argcount = self.arguments[i]
-            self.ptr += 1
-            args = [x for x in self.memory[self.ptr:self.ptr+argcount]]
-            if self.debug:
-                self._dbg([self.names[i]] + [str(x) for x in args])
-                #self._dbg([str(x) for x in self.register])
-            res = self.instructionset[i](self, *args)
-            if res:                 # jump
-                self.ptr = res
-            else:                   # no jump, implicit return of None
-                self.ptr += argcount
 
-with open ("challenge.bin", "rb") as datafile:
-    VM = vm()
-    data = datafile.read()
-    memory = [x for x in struct.unpack("<" + "H" * int(0.5 * len(data)), data)]
-    VM.loadmemory(memory)
+        while self.running:
+            # current instruction
+            instruction = self.memory[self.ptr]
+            func, argcount, name = self.instructions[instruction]
+            args = self.memory[self.ptr + 1:self.ptr + argcount + 1]
+            if self.ptr not in self.known_instructions:
+                self.known_instructions[self.ptr] = self._create_debug_string(name, args)
+            if self.debug:
+                self._dbg(name, args)
+            res = func(self, *args)
+            # if result is a location, jump to it, otherwise go to next instruction
+            self.ptr = res if res else self.ptr + argcount + 1
+
+        self._dump_known_instructions()
+
+with open("challenge.bin", "rb") as f:
+    rom = f.read()
+    mem = list(struct.unpack("<" + "H" * (len(rom) / 2), rom))
+    VM = Synacor(mem, debug=False)
     try:
         VM.run()
     except KeyboardInterrupt:
